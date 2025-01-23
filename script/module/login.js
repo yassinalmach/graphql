e5ff0bed32a7ae98bb325cd5f4e472ffa3a63b91
yelmach <yassineelmach0@gmail.com>
2025-01-23 20:26:27 +0100

    add login page

---
+++ script/module/login.js
@@ -0,0 +1,37 @@
+import { setloginToken } from "./api/auth.js";
+import { showProfile } from "./profile.js";
+import { convertToBase64 } from "./utils.js";
+
+export const showLogin = () => {
+    const app = document.getElementById('app');
+    app.innerHTML = /*html*/`
+        <div class="login-container">
+            <form id="loginForm"class="login-form">
+                <h1>Login</h1>
+                <input type="text" id="identifier" placeholder="Username or Email" required>
+                <input type="password" id="password" placeholder="Password" required>
+                <div id="errorMessage" class="error-message"></div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    `;
+    setupSubmitForm();
+
+};
+
+const setupSubmitForm = () => {
+    document.getElementById('loginForm').addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const identifier = document.getElementById('identifier').value;
+        const password = document.getElementById('password').value;
+        const errorMessage = document.getElementById('errorMessage');
+
+        try {
+            const credentials = convertToBase64(`${identifier}:${password}`)
+            await setloginToken(credentials);
+            showProfile();
+        } catch (error) {
+            errorMessage.textContent = 'Invalid credentials.';
+        }
+    });
+}
