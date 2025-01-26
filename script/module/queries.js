5a8212ac0bf259600674ebea9095dad733428593
yelmach <yassineelmach0@gmail.com>
2025-01-26 23:01:40 +0100

    update queries

--- script/module/queries.js
+++ script/module/queries.js
@@ -1,23 +1,25 @@
 export const USER_INFO_QUERY = `
 {
-    user {
-        lastName
-        firstName
+  user {
+    lastName
+    firstName
+    auditRatio
+    totalDown
+    totalUp
+    transactions(
+      where: {
+        type: {_eq: "level"},
+        _or: [{object: {type: {_eq: "project"}}}, {object: {type: {_eq: "piscine"}}}]
+      }
+      order_by: {amount: desc}
+      limit: 1
+    ) {
+      amount
     }
-}
-`;
-
-export const TOTAL_XP_QUERY = `
-{
+  }
   transaction(
-    where: {
-      type: {_eq: "xp"},
-      _or: [
-        {object: {type: {_eq: "project"}}},
-        {object: {type: {_eq: "piscine"}}},
-        {path: {_ilike: "%module/checkpoint%"}}
-      ]
-    }
+    where: {type: {_eq: "xp"},
+      _or: [{object: {type: {_eq: "project"}}}, {object: {type: {_eq: "piscine"}}}, {path: {_ilike: "%module/checkpoint%"}}]}
   ) {
     amount
     path
@@ -27,31 +29,36 @@ export const TOTAL_XP_QUERY = `
     }
   }
 }
-`
+`;
 
-export const AUDIT_RATIO_QUERY = `
+export const XP_PROGRESS_QUERY = `
 {
-  user {
-    auditRatio
-    totalDown
-    totalUp
-  }
-}
-`
-
-export const CURRENT_LEVEL_QUERY = `
-{
-  user {
-    id
-    transactions(
-      where: {type: {_eq: "level"}, 
-        _or: [
-          {object: {type: {_eq: "project"}}},
-          {object: {type: {_eq: "piscine"}}}
-        ]}
-      limit: 1
-    ) {
-      amount
+  transaction(
+    where: {
+      type: {_eq: "xp"},
+      _or: [{object: {type: {_eq: "project"}}}, {object: {type: {_eq: "piscine"}}}]
+    }
+    order_by: {createdAt: asc}
+  ) {
+    amount
+    createdAt
+    object {
+      name
     }
   }
-}`
+}
+`;
+
+export const SKILL_QUERY = `
+{
+  transaction(
+    where: {
+      type: {_ilike: "%skill%"}
+    }
+    order_by: {amount: desc}
+  ) {
+    type
+    amount
+  }
+}
+`;
